c33b1145c2efc87273d9867d3215ea0db2dd067e
Denny Mbenwa <ennakili@aim.com>
2026-04-05 00:23:42 -0500

    Initial commit.

--- Chapter 2/app/index.js
+++ Chapter 2/app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World from here!");
 });
 
 const server = app.listen(3000, () => {
